30115c60dc2463d0f672c6946fe61105669aefbb
Clester31 <christianlester114@gmail.com>
2024-07-08 18:13:25 -0400

    fixed issue with routing

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ export default function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/onsale' element={<Store shopCategory={allItems} banner={'On Sale'} />} />
-          <Route path='/new' element={<Store shopCategory={allItems} banner={'New Arrivals'} />} />
-          <Route path='/classics' element={<Store shopCategory={allItems} banner={'Classics'} />} />
-          <Route path='/popular' element={<Store shopCategory={allItems} banner={'Popular'} />} />
+          <Route path='mock-record-store/' element={<Home />} />
+          <Route path='mock-record-store/cart' element={<Cart />} />
+          <Route path='mock-record-store/onsale' element={<Store shopCategory={allItems} banner={'On Sale'} />} />
+          <Route path='mock-record-store/new' element={<Store shopCategory={allItems} banner={'New Arrivals'} />} />
+          <Route path='mock-record-store/classics' element={<Store shopCategory={allItems} banner={'Classics'} />} />
+          <Route path='mock-record-store/popular' element={<Store shopCategory={allItems} banner={'Popular'} />} />
         </Routes>
       </BrowserRouter>
     </div>
